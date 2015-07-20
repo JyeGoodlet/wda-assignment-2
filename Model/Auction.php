9c8da54df60a088a184f5344e4af6ee6821882fb
jgoodlet <jye.goodlet@gmail.com>
2015-07-20 23:36:38 +1000

    Model Folder Auction Item Class

---
+++ Model/Auction.php
@@ -0,0 +1,30 @@
+<?php
+
+class AuctionItem {
+
+	#int id of auction
+	public $id;
+	#name of auction item
+	public $name;
+	#description of item
+	public $description;
+	#creation date of auction
+	public $createdDate
+	#start date of auction
+	public $startDate;
+	#end date of auction
+	public $endDate;
+	#double staring dollar amount of auction
+	public $startBid;
+	#double reserve price of auction
+	public $reservePrice;
+	#current winning bid id
+	public $winningBidId
+
+
+
+}
+
+
+
+?>
