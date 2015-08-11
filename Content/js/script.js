a5715af165a5e765b995b849f2c34d7015ae340d
Ash Rowe <ashley.rowe@iinet.net.au>
2015-08-11 11:13:16 +1000

    Added accordion sidebar menu to admin view.

--- Content/js/script.js
+++ Content/js/script.js
@@ -1,16 +1,28 @@
 ﻿
 
 //Pagination for threads.php 
-$(document).ready(function() {
+$(document).ready(function () {
+
     $('#subcategoryThreads').DataTable({
     "bSort": false
-  } );
-} );
-
+    });
+    
 //pagination for thread.php comments
-$(document).ready(function() {
+
     $('#threadComments').DataTable({
     "bSort": false
-  } );
-} );
+    });
+
+    $(function () {
+        $('.accordion').accordion({
+            collapsible: true
+        });
+        $('.accordion2').accordion({
+            collapsible: true
+        });
+    });
+
+
+});
+
 
