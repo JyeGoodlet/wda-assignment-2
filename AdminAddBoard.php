cf1e6bdaa3d5608fa16002d36baea854970c79ca
Ash Rowe <ashley.rowe@iinet.net.au>
2015-08-12 23:12:34 +1000

    Admin add category and edit category implemented

---
+++ AdminAddBoard.php
@@ -0,0 +1,45 @@
+<?php
+require "/Model/ModelFacade.php";
+//redirect if user not logged in as admin
+
+ModelFacade::redirectUnauthorisedNotAdmin();
+
+//get all categories and subcategories
+
+OnRequest();
+
+function OnRequest() {
+
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+        if(isset($_POST['categoryName'])) {
+            AdminAddBoard($_POST['categoryName']);
+        }
+    }
+    else {        
+        include_once('/Views/Admin/AddBoard.html');
+    }
+
+}
+
+function AdminAddBoard($categoryName) {
+    $categoryName = htmlspecialchars($categoryName);
+    $result = ModelFacade::AdminAddBoard($categoryName);
+    
+    if($result) {
+        switch($result[0]) {
+            case 0:
+                $success = "Board " . $categoryName . " successfully added!";
+                break;
+            default:
+                $error = "There was an error adding " . $categoryName . ": code = " . $result[0];
+                break;
+        }
+    }
+    else {
+        $error = $categoryName . " already exists!";
+    }
+
+    include_once('/Views/Admin/AddBoard.html');
+}
+
+?>
